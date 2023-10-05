536cc6a6df57218ce0326e9af2311f98beb1043e
nishan <nishanpradhan500@gmail.com>
2023-10-05 23:44:58 +0545

    implement  admin_dash,student_dash,auth_route

--- app.js
+++ app.js
@@ -15,41 +15,41 @@ app.use(express.static("public/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define routes and their handlers
 
-// Render the index page
-app.get("/dash", async (req, res) => {
-    res.render("index");
-});
+// Define routes and their handlers
+//auth route page
 app.get("/", async (req, res) => {
     res.render("auth/login");
 });
-
-// Render the dashboard page
-app.get("/dashboard", async (req, res) => {
-    const studentCount = await studentsmanagement.findAll(); // Retrieve all students
-    res.render("dashboard", { allstudents: studentCount.length });
-});
-
-// Render the createStudents form page
-app.get("/createstudents", (req, res) => {
-    res.render("createStudents");
-});
-
-// Render the studentlists page 
-app.get("/studentlists", (req, res) => {
-    res.render("viewstudent"); // Check the correct path for rendering
-});
-
-// Render the library page 
-app.get("/library", (req, res) => {
-    res.render("library"); // Check the correct path for rendering
-});
-
-// Render the library page 
+// Handle the update route (you need to implement this)
 app.get("/login", (req, res) => {
     res.render("auth/login"); // Check the correct path for rendering
 });
+
+
+
+//admin route page
+app.get("/admin", async (req, res) => {
+    res.render("admin/admin");
+});
+// Render the dashboard page
+app.get("/admin/dashboard", async (req, res) => {
+    const studentCount = await studentsmanagement.findAll(); // Retrieve all students
+    res.render("admin/addashboard", { allstudents: studentCount.length });
+});
+// Render the createStudents form page
+app.get("/admin/createstudents", (req, res) => {
+    res.render("admin/createStudents");
+});
+// Render the studentlists page 
+app.get("/admin/studentlists", (req, res) => {
+    res.render("admin/viewstudent"); // Check the correct path for rendering
+});
+// Render the admin library page 
+app.get("/admin/library", (req, res) => {
+    res.render("admin/adlibrary"); // Check the correct path for rendering
+});
+
 
 // Handle POST request to add a new student
 app.post("/createstudent", async (req, res) => {
@@ -63,16 +63,45 @@ app.post("/createstudent", async (req, res) => {
         bloodgroup,
     });
 
-    res.redirect("/studentlists");
+    res.redirect("/admin/studentlists");
 });
-
-// Handle the update route (you need to implement this)
-
 // Handle GET request to update a specific student
 app.get("/update/:id", (req, res) => {
     const studentId = req.params.id;
     console.log("Updating student with ID:", studentId);
 });
+
+
+
+//student route page
+// Render the index page
+app.get("/student", async (req, res) => {
+    res.render("student/index");
+});
+// Render the dashboard page
+app.get("/student/dashboard", async (req, res) => {
+    const studentCount = await studentsmanagement.findAll(); // Retrieve all students
+    res.render("student/addashboard", { allstudents: studentCount.length });
+});
+// Render the library page 
+app.get("/student/library", (req, res) => {
+    res.render("student/library"); // Check the correct path for rendering
+});
+// Render the onlineclass page 
+app.get("/student/onlineclass", (req, res) => {
+    res.render("student/onlineclass"); // Check the correct path for rendering
+});
+// Render the library page 
+app.get("/student/homework", (req, res) => {
+    res.render("student/homework"); // Check the correct path for rendering
+});
+
+
+
+
+
+
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
